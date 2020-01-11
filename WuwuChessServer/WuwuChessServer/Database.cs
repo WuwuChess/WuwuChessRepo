68008c8fb326384cdd058f826d7ec4c0a7163ff6
kuuhakuj <1241452188@qq.com>
2020-01-11 23:33:50 +0800

    服务器访问数据库代码整理

--- WuwuChessServer/WuwuChessServer/Database.cs
+++ WuwuChessServer/WuwuChessServer/Database.cs
@@ -1,26 +1,91 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Data.OleDb;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using MySql.Data.MySqlClient;
+using System.Data;
+using WuwuChessServer;
 
-namespace WuwuChessServer
+namespace Database
 {
-    string connectionStringStr;
-    string sqlStr;
-
-
-    class Login //用户登录操作
+    class SqlConnector
     {
-        public static string GetConnectionString()
+        string connectionStringStr;
+        string sqlStr;
+        public SqlConnector()
         {
-            return "Data Source = 127.0.0.1;Database=wuwuchess;UserID = root;Password=lzjlzq33";
+            connectionStringStr = "Data Source = 127.0.0.1;Database=wuwuchess;UserID = root;Password=lzjlzq33";
         }
 
+        public bool Check_ID(string id)  //判断用户名是否与数据库中已有的重复或为空，未重复返回true，重复返回false
+        {
+            sqlStr = "select * from wuwuchess.user where id = " + id + ";";
+            MySqlConnection cnn = null;
+            MySqlDataAdapter adapter = null;
+            DataTable dt = null;
+            try
+            {
+                cnn = new MySqlConnection(connectionStringStr);
+                cnn.Open();
+
+                adapter = new MySqlDataAdapter(sqlStr, cnn);
+                DataSet ds = new DataSet();
+
+                if (adapter.Fill(ds) > 0)
+                {
+                    dt = ds.Tables[0];
+                    cnn.Close();
+                    if (id == dt.Columns[0].ToString())//如果数据库中已经存在该用户id
+                    {
+                        return false;
+                    }
+                    return true;
+                }
+                else
+                {
+                    cnn.Close();
+                    return true;
+                }
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine(error.Message);
+                return false;
+            }
+        }
+
+        public void Set_account(string id, string password, string name)  //向数据库中写入用户名、昵称和密码
+        {
+            if (Check_ID(id))
+            {
+                MySqlConnection cnn = null;
+                MySqlCommand cmd = null;
+                sqlStr = "insert into user(id,name,password) values('" + id + "','" + name + "','" + password + "');";
+                int result = -1;
+                try
+                {
+                    cnn = new MySqlConnection(connectionStringStr);
+                    cnn.Open();
+
+                    cmd = new MySqlCommand();
+                    cmd.Connection = cnn;
+                    cmd.CommandText = sqlStr;
+
+                    result = cmd.ExecuteNonQuery();
+                }
+                catch (Exception error)
+                {
+                    System.Console.WriteLine(error.Message);
+                }
+            }
+        }
         public bool Check(string id, string password)  //与数据库对比，判断账号密码是否正确
         {
             sqlStr = "select * from wuwuchess.user where id = " + id + " and password = " + password + ";";
             MySqlConnection cnn = null;
             MySqlDataAdapter adapter = null;
-            connectionStringStr = GetConnectionString();
             try
             {
                 cnn = new MySqlConnection(connectionStringStr);
@@ -37,7 +102,6 @@ namespace WuwuChessServer
                 else
                 {
                     cnn.Close();
-                    MessageBox.Show("输入的账号或密码有误，请重新输入");
                     return false;
                 }
             }
@@ -56,7 +120,6 @@ namespace WuwuChessServer
             MySqlConnection cnn = null;
             MySqlDataAdapter adapter = null;
             DataTable dt = null;
-            connectionStringStr = GetConnectionString();
 
             MySqlDataAdapter adapter1 = null;
             DataTable dt1 = null;
@@ -97,6 +160,5 @@ namespace WuwuChessServer
             }
             return user;
         }
-
     }
 }
