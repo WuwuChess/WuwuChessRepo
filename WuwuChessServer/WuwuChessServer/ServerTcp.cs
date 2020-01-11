633f8ad248498bb8644d4413ac70515c29ba20df
kuuhakuj <1241452188@qq.com>
2020-01-11 23:44:08 +0800

    服务器登录、注册、查重

--- WuwuChessServer/WuwuChessServer/ServerTcp.cs
+++ WuwuChessServer/WuwuChessServer/ServerTcp.cs
@@ -8,11 +8,13 @@ using System.IO;
 using System.Threading;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using Database;
 /// <summary>
 /// 状态码说明
 /// 200 成功
 /// 404 未知信息
 /// 901 用户名或密码错误
+/// 902 用户名重复
 /// </summary>
 namespace ServerTcp
 {
@@ -20,6 +22,7 @@ namespace ServerTcp
     class HttpThreadHandler
     {
         public TcpListener myListener;
+        public SqlConnector connector;
         public void HandleThread()
         {
             TcpClient client = myListener.AcceptTcpClient();
@@ -31,52 +34,71 @@ namespace ServerTcp
             string requestData = Encoding.ASCII.GetString(data, 0, bytes);
             Console.WriteLine(requestData);//在控制台中查看报文内容
 
-            string[] lines=requestData.Split("\n");
-            if(lines.Length==0)
+            string[] lines = requestData.Split("\n");
+            if (lines.Length == 0)
                 return;
-            string[] first_line=lines[0].Split(" ");
-            if(first_line.Length==0||!first_line[0].Equals("POST"))
+            string[] first_line = lines[0].Split(" ");
+            if (first_line.Length == 0 || !first_line[0].Equals("POST"))
                 return;
-            JObject postData=JsonConvert.DeserializeObject(lines[lines.Length-1]) as JObject;
-            string type=(string)postData["type"];
+            JObject postData = JsonConvert.DeserializeObject(lines[lines.Length - 1]) as JObject;
+            string type = (string)postData["type"];
             string responseData = "";
-            switch(type){
+            switch (type)
+            {
                 case "login":
-                    string userName=(string)postData["username"];
-                    string password=(string)postData["password"];
-                    bool check;//TODO:插入数据库查询接口
-                    if (check)
                     {
-                        responseData = "200";
-                        string listenerIp = (string)postData["listenerip"];
-                        //TODO:修改用户类中的监听ip地址
+                        string userName = (string)postData["username"];
+                        string password = (string)postData["password"];
+                        bool check=connector.Check(userName,password);//TODO:插入数据库查询接口
+                        if (check)
+                        {
+                            responseData = "200";
+                            string listenerIp = (string)postData["listenerip"];
+                            //TODO:修改用户类中的监听ip地址
+                        }
+                        else
+                        {
+                            responseData = "901";
+                        }
                     }
-                    else
-                    {
-                        responseData = "901";
-                    }
-                break;
+                    break;
                 case "register":
-                    string userName = (string)postData["username"];
-                    string password = (string)postData["password"];
-                    string nickName = (string)postData["nickname"];
+                    {
+                        string userName = (string)postData["username"];
+                        string password = (string)postData["password"];
+                        string nickName = (string)postData["nickname"];
+                        connector.Set_account(userName, password, nickName);
+                        responseData = "200";
+                    }
                     break;
                 case "search":
-                break;
+                    break;
                 case "create":
-                break;
+                    break;
                 case "ready":
-                break;
+                    break;
                 case "retract":
-                break;
+                    break;
                 case "chat":
-                break;
+                    break;
                 case "move":
-                break;
+                    break;
                 case "check":
+                    {
+                        string userName = (string)postData["username"];
+                        bool check=connector.Check_ID(userName);
+                        if (check)
+                        {
+                            responseData = "200";
+                        }
+                        else
+                        {
+                            responseData = "902";
+                        }
+                    }
                     break;
                 default:
-                break;
+                    break;
             }
             int iStartPos = requestData.IndexOf("HTTP", 1);
             string sHttpVersion = requestData.Substring(iStartPos, 8);
@@ -132,12 +154,14 @@ namespace ServerTcp
         //连接服务器数据库的接口
         string[] userNames;
         TcpListener listener;
+        SqlConnector myConnector;
         int port = 2110;
         public Listener()
         {
-            listener = new TcpListener(IPAddress("127.0.0.1"), port);
+            myConnector = new SqlConnector();
+            listener = new TcpListener(IPAddress.Parse("127.0.0.1"), port);
             listener.Start();
-            Console.WriteLine("WuwuChess server started at http://{0}.\n", myHttpListener.LocalEndpoint);
+            Console.WriteLine("WuwuChess server started at http://{0}.\n", listener.LocalEndpoint);
 
             while (true)
             {
@@ -148,6 +172,7 @@ namespace ServerTcp
                 //派生线程，处理用户请求
                 HttpThreadHandler handler = new HttpThreadHandler();
                 handler.myListener = listener;
+                handler.connector = myConnector;
                 ThreadStart threadStart = new ThreadStart(handler.HandleThread);
                 Thread handlerThread = new Thread(threadStart);
                 handlerThread.Name = "Created at " + DateTime.Now.ToString();
